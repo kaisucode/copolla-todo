8539c5064c68fba6c9fed6fa83b249cd6b1f0328
Kevin Hsu <kevin.hsukaihao@gmail.com>
2020-08-19 17:59:18 -0700

    Minimal example for electron file IO

--- data/test.js
+++
@@ -1,3 +0,0 @@
-const fs = require('fs');
-try { fs.writeFileSync('myfile.txt', 'the text to write in the file', 'utf-8'); }
-catch(e) { alert('Failed to save the file !'); }
---
+++ main.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const fs = require('fs');
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  win.loadFile('public/index.html');
+  win.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
+
+ipcMain.on('blah', (event, arg) => {
+  console.log(arg);
+  try { fs.writeFileSync('public/data/myfile.txt', JSON.stringify(arg), 'utf-8'); }
+  catch(e) { alert('Failed to save the file !'); }
+})
