7a7453fca90eba513988c988c4e433f9d9c6b719
Kevin Hsu <kevin.hsukaihao@gmail.com>
2020-08-21 15:24:30 -0700

    JSON schema

--- project-project-vue/src/store/index.js
+++ project-project-vue/src/store/index.js
@@ -5,7 +5,36 @@ Vue.use(Vuex)
 
 const store = new Vuex.Store({
   state: {
-    count: 0
+    count: 0, 
+    todo: {
+      "week": {
+        2020: {
+          8: [
+            [{ "taskName": "play theland", "taskDescription": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt", "category": "gaming", "subtasks": [ { "subtaskName": "kill 3 animals", "completed": true }, { "subtaskName": "kill 2 animals", "completed": false } ] }], 
+            [], 
+            [], 
+            [], 
+            [], 
+            [], 
+            []
+          ]
+        }
+      }, 
+      "month": {
+        2020: {
+          8: ["blah", "blah", "blah", "blah", "blah", "blah"]
+        }
+      }, 
+      "year": {
+        2020: ["blah", "blah", "blah", "blah", "blah", "blah", "blah", "blah", "blah", "blah", "blah", "blah"]
+      }, 
+      "categories": [
+        {"name": "CS_eg", "style": "green", "tasks": [{"name": "project-project"}]}, 
+        {"name": "Art_eg", "style": "pink", "tasks": [{"name": "project-project"}]}, 
+        {"name": "unlisted", "style": "brown", "tasks": [{"name": "project-project"}]}
+      ], 
+      "back-burner": []
+    },  
   },
   mutations: {
     increment (state) {
