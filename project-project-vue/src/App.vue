f92ac2053c61cb80920794f70af01e21bc961101
Kevin Hsu <kevin.hsukaihao@gmail.com>
2020-08-21 13:15:01 -0700

    About to attempt module scss

--- project-project-vue/src/App.vue
+++ project-project-vue/src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">This Week</router-link> |
+      <router-link to="/about">This Month</router-link> | 
+      <router-link to="/about">This Year</router-link> | 
+      <router-link to="/about">Categories</router-link>
     </div>
     <router-view/>
   </div>
---
+++ project-project-vue/src/views/Home/index.vue
@@ -0,0 +1,50 @@
+<template>
+	<div class="home">
+		<div class="container">
+			<div class="row">
+				<TextCard title="Mon" />
+				<TextCard title="Tue"/>
+				<TextCard title="Wed" />
+				<TextCard title="Thu"/>
+			</div>
+			<div class="row">
+				<TextCard title="Fri" />
+				<TextCard title="Sat"/>
+				<TextCard title="Sun" />
+				<TextCard title="Back-burner"/>
+			</div>
+		</div>
+		<!-- <button @click="increment">+</button> -->
+	</div>
+</template>
+
+<script>
+// @ is an alias to /src
+import TextCard from '@/components/TextCard.vue'
+
+export default {
+	name: 'Home',
+	components: {
+      TextCard
+	}, 
+	methods: {
+		// increment(){
+		//     this.$store.commit('increment');
+		// }
+	}
+}
+</script>
+
+<style scoped lang="scss">
+.container{
+  display: flex;
+	flex-direction: column;
+	justify-content: center;
+
+	.row{
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+	}
+}
+</style>
