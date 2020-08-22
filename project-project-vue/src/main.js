106c102d189c2d0698cbf216ef3a1ee645f15eaa
Alek Westover <alek.westover@gmail.com>
2020-08-21 21:47:31 -0400

    yay

--- project-project-vue/src/main.js
+++ project-project-vue/src/main.js
@@ -10,3 +10,47 @@ new Vue({
   store, 
   render: h => h(App)
 }).$mount('#app');
+
+
+const KEY_CODES = {
+  "h": 72,
+  "j": 74, 
+  "k": 75, 
+  "l": 76, 
+  "d": 68, 
+  "f": 70,
+  "a": 65, 
+  "i": 73
+};
+
+function currentPage(){
+  return window.location.href.substring(window.location.href.lastIndexOf('/') + 1);
+}
+
+document.addEventListener("keydown", (event) => {
+  if (event.keyCode == KEY_CODES["h"]){
+    
+  }
+  else if (event.keyCode == KEY_CODES["j"]){
+    
+  }
+  else if (event.keyCode == KEY_CODES["k"]){
+    
+  }
+  else if (event.keyCode == KEY_CODES["l"]){
+    
+  }
+  else if (event.keyCode == KEY_CODES["d"]){
+    router.push("year");
+  }
+  else if (event.keyCode == KEY_CODES["f"]){
+    
+  }
+  else if (event.keyCode == KEY_CODES["a"]){
+    
+  }
+  else if (event.keyCode == KEY_CODES["i"]){
+    
+  }
+});
+
