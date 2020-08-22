6f890f29fec101727e26724c374e44d9b099c7d9
Alek Westover <alek.westover@gmail.com>
2020-08-22 18:52:16 -0400

    alek

--- project-project-vue/src/main.js
+++ project-project-vue/src/main.js
@@ -63,6 +63,10 @@ const GRID_SIZES = {
   "year": {"rows": 3, "cols": 4}
 };
 
+function inToDoPage(){
+  return ["week", "month", "year"].includes(getCurrentPage());
+}
+
 document.addEventListener("keydown", (event) => {
   let key_down = "";
   let grid_size = GRID_SIZES[getCurrentPage()];
@@ -73,156 +77,160 @@ document.addEventListener("keydown", (event) => {
     }
   }
 
-  if (key_down == "") { // unrecognized key pressed 
+  if (key_down == "") // unrecognized key pressed 
     return;
-  }
 
-  if (key_down == "ENTER"){
-    zoomed_in = true;
-    curPage = getCurrentPage();
-    focused_textcard_idx = focus_coord.row * grid_size.cols + focus_coord.col;
-
-    if(curPage == "week")
-      focused_task_time = "2020-8-1";
-    else if(curPage == "month")
-      focused_task_time = "2020-8";
-    else if(curPage == "year")
-      focused_task_time = "2020";
-    else if(curPage == "category")
-      alert("I can't");
-
-    focused_tasks = store.state.todo[curPage][focused_task_time][focused_textcard_idx];
-    focused_task_idx = 0;
-    focused_task_id = `#textcard_${focus_coord.row}_${focus_coord.col}`;
-  }
-  if (key_down == "ESCAPE"){
-    zoomed_in = false;
-    $($(focused_task_id).find('li')[focused_task_idx]).removeClass("kevinFocus");
-  }
-
-  if(zoomed_in) {
-    if("jk".includes(key_down)){
-      $($(focused_task_id).find('li')[focused_task_idx]).removeClass("kevinFocus");
-
-      if (key_down == "j")
-        focused_task_idx = (focused_task_idx + 1) % focused_tasks.length;
-      else if (key_down == "k") 
-        focused_task_idx = (focused_task_idx - 1 + focused_tasks.length) % focused_tasks.length;
-
-      $($(focused_task_id).find('li')[focused_task_idx]).addClass("kevinFocus");
-    }
-
-    // as of right now ipdxa are kind of sketched out, but we don't update what
-    // stuff looks like, and writeData is not written
-
-    else if (key_down == "i"){
-      (async (store) => {
-        // input: 'textarea'
-        const {value: new_task_name } = await Swal.fire({
-          input: 'text',
-          inputPlaceholder: 'change task name',
-          inputAttributes: {
-            'aria-label': 'new task name'
-          },
-          icon: 'error',
-          showCancelButton: true
-        });
-
-        store.commit("editTask", {
-          "curPage": curPage,
-          "focused_task_time": focused_task_time,
-          "focused_textcard_idx": focused_textcard_idx,
-          "focused_task_idx": focused_task_idx, 
-          "new_task_name": new_task_name
-        });
-        writeData();
-      })(store);
-
-    }
-    else if (key_down == "d"){
-      store.commit("deleteTask", {
-        "curPage": curPage,
-        "focused_task_time": focused_task_time,
-        "focused_textcard_idx": focused_textcard_idx,
-        "focused_task_idx": focused_task_idx
-      });
-      writeData();
-    }
-    else if (key_down == "a"){
-      (async (store) => {
-        const {value: new_task_name } = await Swal.fire({
-          input: 'text',
-          inputPlaceholder: 'new task name?',
-          inputAttributes: {
-            'aria-label': 'Type your message here'
-          },
-          icon: 'info',
-          showCancelButton: true
-        });
-        if (new_task_name) {
-          console.log(new_task_name, curPage, focused_task_time, focused_textcard_idx);
-          store.commit("pushTask", {
-            "curPage": curPage, 
-            "focused_task_time": focused_task_time, 
-            "focused_textcard_idx": focused_textcard_idx, 
-            "task": {
-              "taskName": new_task_name, 
-              "category": "gaming", 
-              "subtasks": [] 
-            }
-          });
-          writeData();
-        }
-      })(store);
-    }
-    else if (key_down == "x"){
-      // Swal.fire({
-      //   title: 'Attempting to press x!',
-      //   text: 'hmm?',
-      //   icon: 'success',
-      //   confirmButtonText: 'LIT!!!!'
-      // });
-      copied_task = store.state.todo[curPage][focused_task_time][focused_textcard_idx][focused_task_idx]; 
-      store.commit("deleteTask", {
-        "curPage": curPage,
-        "focused_task_time": focused_task_time,
-        "focused_textcard_idx": focused_textcard_idx,
-        "focused_task_idx": focused_task_idx
-      });
-
-      writeData();
-    }
-    else if (key_down == "p"){
-      store.commit("insertTask", {
-        "curPage": curPage, 
-        "focused_task_time": focused_task_time, 
-        "focused_textcard_idx": focused_textcard_idx, 
-        "focused_task_idx": focused_task_idx,
-        "task": copied_task
-      });
-      writeData();
-    }
-  }
-  else if(!zoomed_in) {
+  if(!zoomed_in) {
     if (key_down == "d")
       router.push(pages[(pages.indexOf(getCurrentPage())-1 + pages.length) % pages.length]);
     else if (key_down == "f")
       router.push(pages[(pages.indexOf(getCurrentPage())+1) % pages.length]);
-    if("hjlk".includes(key_down)){
-      $(`#textcard_${focus_coord.row}_${focus_coord.col}`).removeClass("alekFocus");
-
-      if (key_down == "h")
-        focus_coord.col = (focus_coord.col - 1 + grid_size.cols) % grid_size.cols;
-      else if (key_down == "j")
-        focus_coord.row = (focus_coord.row - 1 + grid_size.rows) % grid_size.rows;
-      else if (key_down == "k")
-        focus_coord.row = (focus_coord.row + 1) % grid_size.rows;
-      else if (key_down == "l")
-        focus_coord.col = (focus_coord.col + 1) % grid_size.cols;
-
-      $(`#textcard_${focus_coord.row}_${focus_coord.col}`).addClass("alekFocus");
-    }
   }
 
+  if(inToDoPage()){
+    if (key_down == "ENTER"){
+      zoomed_in = true;
+      curPage = getCurrentPage();
+      focused_textcard_idx = focus_coord.row * grid_size.cols + focus_coord.col;
+
+      if(curPage == "week")
+        focused_task_time = "2020-8-1";
+      else if(curPage == "month")
+        focused_task_time = "2020-8";
+      else if(curPage == "year")
+        focused_task_time = "2020";
+      else if(curPage == "category")
+        alert("I can't");
+
+      focused_tasks = store.state.todo[curPage][focused_task_time][focused_textcard_idx];
+      focused_task_idx = 0;
+      focused_task_id = `#textcard_${focus_coord.row}_${focus_coord.col}`;
+    }
+    if (key_down == "ESCAPE"){
+      zoomed_in = false;
+      $($(focused_task_id).find('li')[focused_task_idx]).removeClass("kevinFocus");
+    }
+
+    if(zoomed_in) {
+      if("jk".includes(key_down)){
+        $($(focused_task_id).find('li')[focused_task_idx]).removeClass("kevinFocus");
+
+        if (key_down == "j")
+          focused_task_idx = (focused_task_idx + 1) % focused_tasks.length;
+        else if (key_down == "k") 
+          focused_task_idx = (focused_task_idx - 1 + focused_tasks.length) % focused_tasks.length;
+
+        $($(focused_task_id).find('li')[focused_task_idx]).addClass("kevinFocus");
+      }
+
+      // as of right now ipdxa are kind of sketched out, but we don't update what
+      // stuff looks like, and writeData is not written
+
+      else if (key_down == "i"){
+        (async (store) => {
+          // input: 'textarea'
+          const {value: new_task_name } = await Swal.fire({
+            input: 'text',
+            inputPlaceholder: 'change task name',
+            inputAttributes: {
+              'aria-label': 'new task name'
+            },
+            icon: 'error',
+            showCancelButton: true
+          });
+
+          store.commit("editTask", {
+            "curPage": curPage,
+            "focused_task_time": focused_task_time,
+            "focused_textcard_idx": focused_textcard_idx,
+            "focused_task_idx": focused_task_idx, 
+            "new_task_name": new_task_name
+          });
+          writeData();
+        })(store);
+
+      }
+      else if (key_down == "d"){
+        store.commit("deleteTask", {
+          "curPage": curPage,
+          "focused_task_time": focused_task_time,
+          "focused_textcard_idx": focused_textcard_idx,
+          "focused_task_idx": focused_task_idx
+        });
+        writeData();
+      }
+      else if (key_down == "a"){
+        (async (store) => {
+          const {value: new_task_name } = await Swal.fire({
+            input: 'text',
+            inputPlaceholder: 'new task name?',
+            inputAttributes: {
+              'aria-label': 'Type your message here'
+            },
+            icon: 'info',
+            showCancelButton: true
+          });
+          if (new_task_name) {
+            console.log(new_task_name, curPage, focused_task_time, focused_textcard_idx);
+            store.commit("pushTask", {
+              "curPage": curPage, 
+              "focused_task_time": focused_task_time, 
+              "focused_textcard_idx": focused_textcard_idx, 
+              "task": {
+                "taskName": new_task_name, 
+                "category": "gaming", 
+                "subtasks": [] 
+              }
+            });
+            writeData();
+          }
+        })(store);
+      }
+      else if (key_down == "x"){
+        // Swal.fire({
+        //   title: 'Attempting to press x!',
+        //   text: 'hmm?',
+        //   icon: 'success',
+        //   confirmButtonText: 'LIT!!!!'
+        // });
+        copied_task = store.state.todo[curPage][focused_task_time][focused_textcard_idx][focused_task_idx]; 
+        store.commit("deleteTask", {
+          "curPage": curPage,
+          "focused_task_time": focused_task_time,
+          "focused_textcard_idx": focused_textcard_idx,
+          "focused_task_idx": focused_task_idx
+        });
+
+        writeData();
+      }
+      else if (key_down == "p"){
+        store.commit("insertTask", {
+          "curPage": curPage, 
+          "focused_task_time": focused_task_time, 
+          "focused_textcard_idx": focused_textcard_idx, 
+          "focused_task_idx": focused_task_idx,
+          "task": copied_task
+        });
+        writeData();
+      }
+    }
+    else if(!zoomed_in) {
+      if("hjlk".includes(key_down)){
+        $(`#textcard_${focus_coord.row}_${focus_coord.col}`).removeClass("alekFocus");
+
+        if (key_down == "h")
+          focus_coord.col = (focus_coord.col - 1 + grid_size.cols) % grid_size.cols;
+        else if (key_down == "j")
+          focus_coord.row = (focus_coord.row - 1 + grid_size.rows) % grid_size.rows;
+        else if (key_down == "k")
+          focus_coord.row = (focus_coord.row + 1) % grid_size.rows;
+        else if (key_down == "l")
+          focus_coord.col = (focus_coord.col + 1) % grid_size.cols;
+
+        $(`#textcard_${focus_coord.row}_${focus_coord.col}`).addClass("alekFocus");
+      }
+    }
+
+  }
 });
 
