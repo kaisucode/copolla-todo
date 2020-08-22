906910f5ee11c4836ff09c31de60690a7cfd29a2
Alek Westover <alek.westover@gmail.com>
2020-08-22 03:22:34 -0400

    asdfasdf

--- electronjs/main.js
+++ electronjs/main.js
@@ -18,12 +18,12 @@ app.whenReady().then(createWindow);
 
 ipcMain.on('writeData', (event, data) => {
   console.log(data);
-  try { fs.writeFileSync('public/data/myfile.txt', JSON.stringify(data), 'utf-8'); }
+  try { fs.writeFileSync('public/data/myfile.txt', JSON.stringify(data)); }
   catch(e) { alert('Failed to save the file !'); }
 });
 
 ipcMain.on('readData', (event) => {
-  let data = fs.readFileSync('public/data/myfile.txt', 'utf-8');
+  let data = fs.readFileSync('public/data/myfile.txt');
   return data;
 });
 
