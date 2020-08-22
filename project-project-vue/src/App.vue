7bc160bf9533c02d097643bfc53d46874b07e810
Alek Westover <alek.westover@gmail.com>
2020-08-21 21:17:19 -0400

    alek broken code

--- project-project-vue/src/App.vue
+++ project-project-vue/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">This Week</router-link> |
+      <router-link to="/week">This Week</router-link> |
       <router-link to="/month">This Month</router-link> | 
       <router-link to="/year">This Year</router-link> | 
       <router-link to="/about">Categories</router-link>
@@ -32,3 +32,56 @@
   }
 }
 </style>
+
+<script charset="utf-8">
+
+import router from 'vue-router'
+/* window.history.push(state) */
+
+const KEY_CODES = {
+  "h": 72,
+  "j": 74, 
+  "k": 75, 
+  "l": 76, 
+  "d": 68, 
+  "f": 70,
+  "a": 65, 
+  "i": 73
+};
+
+function currentPage(){
+  return window.location.href.substring(window.location.href.lastIndexOf('/') + 1);
+}
+
+document.addEventListener("keydown", (event) => {
+  if (event.keyCode == KEY_CODES["h"]){
+    
+  }
+  else if (event.keyCode == KEY_CODES["j"]){
+    
+  }
+  else if (event.keyCode == KEY_CODES["k"]){
+    
+  }
+  else if (event.keyCode == KEY_CODES["l"]){
+    
+  }
+  else if (event.keyCode == KEY_CODES["d"]){
+    /* router.push("year"); */
+    window.history.pushState("year");
+    /* window.location.href = "year"; */
+  }
+  else if (event.keyCode == KEY_CODES["f"]){
+    
+  }
+  else if (event.keyCode == KEY_CODES["a"]){
+    
+  }
+  else if (event.keyCode == KEY_CODES["i"]){
+    
+  }
+});
+
+
+</script>
+
