de19a772c07f0142fef7099436aaa2b80e8e7c02
Alek Westover <alek.westover@gmail.com>
2020-08-30 20:31:02 -0400

    bruh

--- src/views/About/index.vue
+++ src/views/About/index.vue
@@ -2,14 +2,14 @@
   <div :class="Styles.container">
     <h1>About</h1>
 
+    <h3 style="color:red">We take no responsibility for lost todo lists. Accept the impermanence of data.</h3>
+
 		<p>
 		This project was created in the summer of 2020, by <a href="https://awestover.github.io/">Alek Westover</a> and <a href="https://kevinhsu.net/">Kevin Hsu</a>, with the assistance of <a href="https://github.com/ifan2032">Leon Fan</a> and <a href="https://github.com/ziyongcui">Ziyong Cui</a>. 
 		<br><br>
 		The idea was borne out of our obsession for Vim, or more precisely, Neovim, and the multitude of markdown and text todo files littered across our systems. 
 		<br>
 		</p>
-
-
 
   </div>
 </template>
